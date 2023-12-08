853ae87d0348ae9f1b2b4438672928442c8dbd27
LOUKSON16 <153290329+LOUKSON16@users.noreply.github.com>
2023-12-08 17:24:32 +0000

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['22543496961', 'loukson', true],
+  ['22543496961', 'loukson', true], 
   [''] 
 ] //Number of owners
 
